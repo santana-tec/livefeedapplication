b0091ae30df860fe7ce80f47dfe2f6257f8031b5
santana-tec <98412757+santana-tec@users.noreply.github.com>
2022-12-01 13:18:53 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/livefeedapplication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.livefeedapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+
+late List<CameraDescription> _cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  _cameras = await availableCameras();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CameraController cameraController;
+  @override
+  void initState() {
+
+    super.initState();
+    cameraController = CameraController(_cameras[1], ResolutionPreset.high);
+    cameraController.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print('User denied camera access.');
+            break;
+          default:
+            print('Handle other errors.');
+            break;
+        }
+      }
+    });
+
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: CameraPreview(cameraController),
+    );
+  }
+}
